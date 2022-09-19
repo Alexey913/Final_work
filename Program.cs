a456effb990f856d4b18fed985b34b4898020291
Alexey Shevtsov <al913@mail.ru>
2022-09-19 21:49:41 +0300

    Create code for maine algorithm

--- Program.cs
+++ Program.cs
@@ -3,3 +3,42 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
 // На этой ветке будем создавать основной алгоритм
+
+
+string [] ChangeArray (string [] enterArray)
+{
+    int changeSize = 0;
+    int length = enterArray.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (enterArray[i].Length <= 3)
+        {
+            changeSize++;
+        }
+    }
+    if (changeSize == 0)
+    {        
+        string [] printArray = new string [] {"В массиве отсутствуют элементы длиной меньше или равной 3 символам"};
+
+        return (printArray);
+    }
+    else
+    {
+        string [] printArray = new string [changeSize];
+        int count = 0;
+        for (int i = 0; i < changeSize; i++)
+        {
+            for (int j = count; j < length; j++)
+            {
+                if (enterArray[j].Length <= 3)
+                {   
+                    printArray[i] = enterArray[j];
+                    count = j+1;
+                    break;
+                }
+            } 
+        }
+        Console.WriteLine ("Массив с элементами длиной меньше или равной 3 символам:");
+        return (printArray);
+    }
+}
